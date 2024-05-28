8d918ba6d94c76216e1a6b379fbd182e75e5ddbd
Denis <denekurich@gmail.com>
2024-05-28 21:16:45 +0300

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.Writer.Write([]byte("test"))
+		c.Writer.Write([]byte("Hello, world"))
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
